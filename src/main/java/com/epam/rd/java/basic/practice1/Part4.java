d8b63875ddd56df277ace7ca6bdae9193bc5f679
xVxkDGMN <xvxkdgmn@email.com>
2020-11-25 19:52:51 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part4.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -1,9 +1,24 @@
 package com.epam.rd.java.basic.practice1;
 
 public class Part4 {
+    /*
+	 * Write a class that implements functionality of defining the greatest common
+	 * divisor of two whole positive numbers passed to the application as the
+	 * command line parameters.
+	 */
+	public static void main(String[] args) {
+		if (args.length == 2) {
+			int num1 = Integer.parseInt(args[0]);
+			int num2 = Integer.parseInt(args[1]);
 
-    public static void main(String[] args) {
-        
-    }
-
+			while (num1 != num2) {
+				if (num1 > num2)
+					num1 = num1 - num2;
+				else
+					num2 = num2 - num1;
+			}
+			System.out.printf("Greatest common divisor is: %d", num2);
+		} else
+			System.out.println("It should be two number according to the task");
+	}
 }
