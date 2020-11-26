f945eee785b83233f53ce229c50bb780d16b51dd
xVxkDGMN <xvxkdgmn@email.com>
2020-11-26 21:24:25 +0000

    Update src/test/java/com/epam/rd/java/basic/practice1/Part2Test.java

--- src/test/java/com/epam/rd/java/basic/practice1/Part2Test.java
+++ src/test/java/com/epam/rd/java/basic/practice1/Part2Test.java
@@ -1,5 +1,19 @@
 package com.epam.rd.java.basic.practice1;
 
 public class Part2Test {
-
+    public static void main(String[] args) {
+    	/*
+    	 * Write a class that implements functionality of addition 
+    	 * of numbers passed to the application as the command 
+    	 * line parameters.
+    	 */
+        int sum = 0;
+		if (args.length>0) {
+		for(int i = 0; i< args.length; i++)
+		{ 
+		sum = sum + Integer.parseInt(args[i]);
+		}
+		System.out.println("Addition of numbers = " + sum);
+		}
+    }
 }
