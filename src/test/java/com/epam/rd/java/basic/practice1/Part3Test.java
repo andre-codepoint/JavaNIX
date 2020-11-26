86d5362c771673eeaf13c7c144dc144a0cf90992
xVxkDGMN <xvxkdgmn@email.com>
2020-11-26 21:24:53 +0000

    Update src/test/java/com/epam/rd/java/basic/practice1/Part3Test.java

--- src/test/java/com/epam/rd/java/basic/practice1/Part3Test.java
+++ src/test/java/com/epam/rd/java/basic/practice1/Part3Test.java
@@ -1,5 +1,19 @@
 package com.epam.rd.java.basic.practice1;
 
 public class Part3Test {
-
+    public static void main(String[] args) {
+        /*
+		 * Write a class that implements functionality of displaying of the command line
+		 * parameters in console (display using a space between them), the result should
+		 * not end with a space.
+		 */
+		String sum = "";
+		if (args.length > 0) {
+			for (int i = 0; i < args.length; i++) {
+				if (i>0) sum = sum + " ";
+				sum = sum + args[i];
+			}
+			System.out.println(sum);
+		}
+    }
 }
