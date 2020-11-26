63f48686703953c4e50c4f8864c24abd938ba464
xVxkDGMN <xvxkdgmn@email.com>
2020-11-26 21:26:01 +0000

    Update src/test/java/com/epam/rd/java/basic/practice1/Part6Test.java

--- src/test/java/com/epam/rd/java/basic/practice1/Part6Test.java
+++ src/test/java/com/epam/rd/java/basic/practice1/Part6Test.java
@@ -1,5 +1,31 @@
 package com.epam.rd.java.basic.practice1;
 
 public class Part6Test {
+    public static void main(String[] args) {
+		/*
+		 * Write a class that creates an array from n elements and fills it with an
+		 * ascending sequence of prime numbers (2, 3, 5, 7 …). The n number should be
+		 * passed as the command line parameter.
+		 */
+		if (args.length > 0) {
+			int lim = Integer.parseInt(args[0]);
 
+			int i = 0;
+			int num = 0;
+			String primeNumbers = "";
+			for (i = 1; i <= lim; i++) {
+				int counter = 0;
+				for (num = i; num >= 1; num--) {
+					if (i % num == 0) {
+						counter = counter + 1;
+					}
+				}
+				if (counter == 2) {
+					primeNumbers = primeNumbers + i + " ";
+				}
+			}
+			System.out.printf("Prime numbers from 1 to %d are :", lim);
+			System.out.println(primeNumbers);
+		}
+	}
 }
