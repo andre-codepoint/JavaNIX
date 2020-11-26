1be2fa8f009807cc02cfd0b32e921ebdf53964b8
xVxkDGMN <xvxkdgmn@email.com>
2020-11-26 21:25:41 +0000

    Update src/test/java/com/epam/rd/java/basic/practice1/Part5Test.java

--- src/test/java/com/epam/rd/java/basic/practice1/Part5Test.java
+++ src/test/java/com/epam/rd/java/basic/practice1/Part5Test.java
@@ -1,5 +1,22 @@
 package com.epam.rd.java.basic.practice1;
 
 public class Part5Test {
+    public static void main(String[] args) {
+		/*
+		 * Write a class that implements functionality of defining the sum of the digits
+		 * of a whole positive number passed to the application as the command line
+		 * parameter.
+		 */
+		int sum = 0;
+		if (args.length > 0) {
+			int num = Integer.parseInt(args[0]);
+			int n = num;
 
+			while (n != 0) {
+				sum = sum + n % 10;
+				n = n / 10;
+			}
+			System.out.printf("Sum the digits of number %d = %d", num, sum);
+		}
+	}
 }
