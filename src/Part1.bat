bffa19119cf0f16a0fc636127912c46ea8ea0e4e
xVxkDGMN <xvxkdgmn@email.com>
2020-11-27 16:21:46 +0000

    Update src/Part1.bat

--- src/Part1.bat
+++ src/Part1.bat
@@ -1,3 +1,3 @@
-javac  main\java\com\epam\rd\java\basic\practice1\Part1Test.java
-java   -cp main\java; com.epam.rd.java.basic.practice1.Part1Test
+javac  main\java\com\epam\rd\java\basic\practice1\Part1.java
+java   -cp main\java; com.epam.rd.java.basic.practice1.Part1
 pause
