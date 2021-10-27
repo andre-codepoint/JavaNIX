a44bce3b6eabca775708903566672666fdf01724
andrejavabest <snsncps@gmail.com>
2021-10-27 18:15:10 +0300

    6

--- src/src/task03/Task03.java
+++ src/src/task03/Task03.java
@@ -3,8 +3,11 @@ package src.task03;
 Создайте массив чисел и выведите этот массив на экран
 Вывод: 1,2,3,4,7,15
  */
+
+import java.util.stream.Stream;
 public class Task03 {
     public static void main(String[] args) {
-
+        int[] myAray = new int[]{1, 2, 3, 4, 7, 15};
+        Stream.of(myAray).forEach(System.out::println);
     }
 }
