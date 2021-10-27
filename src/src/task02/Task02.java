3248613af6dd59a5f56da45378d77c646a385a04
andrejavabest <snsncps@gmail.com>
2021-10-27 14:46:11 +0300

    2

--- src/src/task02/Task02.java
+++ src/src/task02/Task02.java
@@ -1,4 +1,19 @@
 package src.task02;
 
 public class Task02 {
+    public static void main(String[] args) {
+        Float number1 = Float.valueOf(args[0]);
+        Float number2 = Float.valueOf(args[1]);
+        Float result=null;
+        if (args[2].equals("+"))
+            result = (number1 + number2);
+        if (args[2].equals("-"))
+            result = (number1 - number2);
+        if (args[2].equals("*"))
+            result = (number1 * number2);
+        if (args[2].equals("/"))
+            result = (number1) / (number2);
+        if (result != null )System.out.println(result);
+        else System.out.println("Result is incorrect. Please, check inputs.");
+    }
 }
