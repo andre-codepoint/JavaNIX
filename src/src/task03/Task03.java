0e9920df9eb229ebc39f2c1970da1da92ce36c88
andrejavabest <snsncps@gmail.com>
2021-10-27 15:28:59 +0300

    5

--- src/src/task03/Task03.java
+++ src/src/task03/Task03.java
@@ -1,7 +1,10 @@
 package src.task03;
-
+/*
+Создайте массив чисел и выведите этот массив на экран
+Вывод: 1,2,3,4,7,15
+ */
 public class Task03 {
     public static void main(String[] args) {
-        
+
     }
 }
