704cbc3e29f8babce31c24988ff96de96e134071
andrejavabest <snsncps@gmail.com>
2021-10-28 12:59:33 +0300

    7

--- src/src/task03/Task03.java
+++ src/src/task03/Task03.java
@@ -4,10 +4,25 @@ package src.task03;
 Вывод: 1,2,3,4,7,15
  */
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
 public class Task03 {
     public static void main(String[] args) {
-        int[] myAray = new int[]{1, 2, 3, 4, 7, 15};
-        Stream.of(myAray).forEach(System.out::println);
+        int[] myArray = new int[]{1, 2, 3, 4, 7, 15};
+//        IntStream.of(myAray).forEach(x-> System.out.format("%s, ",x));
+        for(int i = 0;i<myArray.length;i++){
+            System.out.print(myArray[i]);
+            if(i<(myArray.length-1))
+                System.out.print(",");
+        }
+
+        }
     }
-}
+
---
+++ src/src/task04/Task04.java
@@ -0,0 +1,13 @@
+package src.task04;
+/*
+Создайте программу, которая переводит температуру в Фарингейтах
+в температуру по цельсию
+Ввод: 41
+Вывод: 5
+ */
+public class Task04 {
+    public static void main(String[] args) {
+        Double tF = (5.0 / 9.0) * (Double.parseDouble(args[0]) - 32);
+        System.out.println(String.format("%.0f",tF));
+    }
+}
