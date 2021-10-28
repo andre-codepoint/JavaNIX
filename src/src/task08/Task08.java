5dec74bd55c78a160646bf0187477e847afc4f03
andrejavabest <snsncps@gmail.com>
2021-10-28 18:41:58 +0300

    10

--- src/src/task08/Task08.java
+++ src/src/task08/Task08.java
@@ -4,11 +4,18 @@ package src.task08;
  */
 public class Task08 {
     public static void main(String[] args) {
+        System.out.printf(String.format("%3s","X"));
+        for (int j = 1; j <=9 ; j++) {
+             System.out.printf(String.format("%3d",j));
+        }
+        System.out.printf(String.format("%n"));
         for (int i = 1; i <=9; i++) {
+            System.out.printf(String.format("%3d",i));
             for (int j = 1; j <=9 ; j++) {
                 int r = i*j;
-                System.out.println(String.format("%3d x %3d = %3d",i,j,r));
+                System.out.printf(String.format("%3d",r));
             }
+            System.out.printf(String.format("%n"));
         }
     }
 }
